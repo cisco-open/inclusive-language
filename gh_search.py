b7429a9e4460c2b5011fdb6b50467eaa5d8da30e
Anne Gentle <agentle@cisco.com>
2022-05-21 14:32:57 -0500

    Update to allow any file type reports

--- gh_search.py
+++ gh_search.py
@@ -18,29 +18,52 @@ def search_github(keyword, filetype):
         return
     else:
         print(f'You have {rate.remaining}/{rate.limit} API calls remaining')
-        query = f'"{keyword}" org:{config.gh_orgname} in:file extension:{filetype}'
-        result = g.search_code(query, order='desc')
+        if filetype == 'any':
+            query = f'"{keyword}" org:{config.gh_orgname}'
+            result = g.search_code(query, order='desc')
 
-        max_size = 100
-        print(f'Found {result.totalCount} {filetype} file(s) with {keyword}')
-        print(f'Keyword,File type,GitHub URL,File match')
-        if result.totalCount > max_size:
-            result = result[:max_size]
-            try:
-                time.sleep(10)
+            max_size = 100
+            
+            print(f'Found {result.totalCount} file(s) with {keyword}')
+            print(f'Keyword,File type,GitHub URL,File match')
+            if result.totalCount > max_size:
+                result = result[:max_size]
+                try:
+                    time.sleep(10)
+                    for file in result:
+                        path = file.path
+                        actualfiletype = path.rsplit(sep='.')[1]
+                        print(f'{keyword},{actualfiletype},{file.download_url},{file.path}')
+                except Exception as e:
+                    print(e)
+            else:
+                for file in result:
+                    print(f'{keyword},{actualfiletype},{file.download_url},{file.path}')
+
+        else:  
+            query = f'"{keyword}" org:{config.gh_orgname} in:file extension:{filetype}'
+            result = g.search_code(query, order='desc')
+
+            max_size = 100
+            print(f'Found {result.totalCount} {filetype} file(s) with {keyword}')
+            print(f'Keyword,File type,GitHub URL,File match')
+            if result.totalCount > max_size:
+                result = result[:max_size]
+                try:
+                    time.sleep(10)
+                    for file in result:
+                        print(f'{keyword},{filetype},{file.download_url},{file.path}')
+                except Exception as e:
+                    print(e)
+            else:
                 for file in result:
                     print(f'{keyword},{filetype},{file.download_url},{file.path}')
-            except Exception as e:
-                print(e)
-        else:
-            for file in result:
-                print(f'{keyword},{filetype},{file.download_url},{file.path}')
     
 
 if __name__ == '__main__':
     #keyword = input('Enter biased keyword such as master, slave, blacklist, whitelist: ')
-    #filetype = input('Enter extension for files to search within such as py for Python, md for Markdown: ')
-    keyword = 'slave'
-    filetype = 'js'
+    #filetype = input('Enter extension for files to search within such as py for Python, md for Markdown, any for all file types: ')
+    keyword = 'whitelist'
+    filetype = 'any'
     search_github(keyword, filetype)
 
