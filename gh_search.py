1dd1a893ef7660853a77acd1523a84240ac03311
Anne Gentle <agentle@cisco.com>
2022-07-01 09:56:41 -0500

    Adds Enterprise GitHub capability

--- gh_search.py
+++ gh_search.py
@@ -5,12 +5,13 @@ import logging
 import time
 
 ACCESS_TOKEN = config.gh_api_key
+API_ENDPOINT = config.egh_api_endpoint
 
 g = Github(ACCESS_TOKEN)
-
+eg = Github(base_url=API_ENDPOINT, login_or_token=config.egh_api_key)
 
 def search_github(keyword, filetype):
-    logging.basicConfig(filename='searchresultsinfo.log',
+    logging.basicConfig(filename='githubsearchresultsinfo.log',
                         encoding='utf-8', 
                         format='%(asctime)s %(message)s',
                         filemode='w',
@@ -48,7 +49,70 @@ def search_github(keyword, filetype):
                 except Exception as e:
                     print("Error: ", e)
             else:
-                for file in result:
+                for file in searchresults:
+                    path = file.path
+                    try:
+                        actualfiletype = path.rsplit(sep='.')[1]
+                    except Exception as e:
+                        actualfiletype = path.rsplit(sep='/')[1]
+                    print(f'{keyword},{actualfiletype},{file.download_url},{file.path}')
+
+        else:
+            # For queries with an exact file type
+            query = f'"{keyword}" org:{config.gh_orgname} in:file extension:{filetype}'
+            searchresults = g.search_code(query, order='desc')
+            
+            logger.info(f'Found {searchresults.totalCount} entries(s) with {keyword}')
+            print(f'Keyword,File type,GitHub URL,File match')
+             
+            for entry in searchresults:
+                try:
+                    # Try to avoid rate limits
+                    time.sleep(2)
+                    print(f'{keyword},{filetype},{searchresults.html_url},{searchresults.path}')
+                except Exception as e:
+                    print("Error: ", e)
+
+def search_enterprise_github(keyword, filetype):
+    logging.basicConfig(filename='enterprisesearchresultsinfo.log',
+                        encoding='utf-8', 
+                        format='%(asctime)s %(message)s',
+                        filemode='w',
+                        level=logging.INFO)
+    logger = logging.getLogger()
+    rate_limit = eg.get_rate_limit()
+    rate = rate_limit.search
+    if rate.remaining == 0:
+        print(f'You have 0/{rate.limit} search API calls remaining. Reset time: {rate.reset}')
+        reset_timestamp = calendar.timegm(rate.reset.timetuple())
+        sleep_time = reset_timestamp - calendar.timegm(time.gmtime()) + 15  
+        time.sleep(sleep_time)
+        return
+    else:
+        logger.info(f'You have {rate.remaining}/{rate.limit} API calls remaining')
+        if filetype == 'any':
+            query = f'"{keyword}" org:{config.egh_orgname}'
+            searchresults = eg.search_code(query, order='desc')
+            # search_code returns a paginated_list https://pygithub.readthedocs.io/en/latest/utilities.html?highlight=pagination#pagination
+            logger.info(f'Found {searchresults.totalCount} entries with {keyword}')
+            # Prints the column labels
+            print(f'Keyword,File type,GitHub URL,File match')
+
+            for entry in searchresults:
+                try:
+                    time.sleep(2)
+                    # each entry is a ContentFile https://pygithub.readthedocs.io/en/latest/github_objects/ContentFile.html#github.ContentFile.ContentFile
+                    path = entry.path
+                    try:
+                        actualfiletype = path.rsplit(sep='.')[1]
+                    except Exception as e:
+                        # Sometimes the string split would find another character, causing an error
+                        actualfiletype = path.rsplit(sep='/')[1] 
+                    print(f'{keyword},{actualfiletype},{entry.html_url},{entry.path}')
+                except Exception as e:
+                    print("Error: ", e)
+            else:
+                for file in searchresults:
                     path = file.path
                     try:
                         actualfiletype = path.rsplit(sep='.')[1]
@@ -73,14 +137,20 @@ def search_github(keyword, filetype):
                     print("Error: ", e)
 
 if __name__ == '__main__':
+    which_github = input('Enter GitHub for github.com or Enterprise for Enterprise GitHub: ')
     keyword = input('Enter biased keyword such as \"master\", \"slave\", \"blacklist\", \"whitelist\": ')
     filetype = input('Enter extension for files to search within such as \"py\" for Python, \"md\" for Markdown, and enter \"any\" for all file types: ')
     #keyword = 'whitelist'
     #keyword = 'slave'
     #filetype = 'any'
     #filetype = 'py'
-    search_github(keyword, filetype)
-    logging.basicConfig(filename='searchresultsinfo.log',
+    if which_github == "GitHub":
+        search_github(keyword, filetype)
+    elif which_github == "Enterprise":
+        search_enterprise_github(keyword, filetype)
+    else:
+        print("Enter GitHub or Enterprise. Your config.py file must have a token and API endpoint when searching Enterprise GitHub.")
+    logging.basicConfig(filename='allsearchresultsinfo.log',
                         encoding='utf-8', 
                         format='%(asctime)s %(message)s',
                         filemode='w',
