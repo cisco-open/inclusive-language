1949f961b6f2384f8e916b798db40b88eee73b9e
Anne Gentle <agentle@cisco.com>
2022-08-02 17:46:19 -0500

    Update for Enterprise GitHub config

--- gh_search.py
+++ gh_search.py
@@ -92,12 +92,45 @@ def search_enterprise_github(keyword, filetype):
         logger.info(f'You have {rate.remaining}/{rate.limit} API calls remaining')
         if filetype == 'any':
             query = f'"{keyword}" org:{config.egh_orgname}'
+            print("Query is: ", query)
             searchresults = eg.search_code(query, order='desc')
             # search_code returns a paginated_list https://pygithub.readthedocs.io/en/latest/utilities.html?highlight=pagination#pagination
             logger.info(f'Found {searchresults.totalCount} entries with {keyword}')
             # Prints the column labels
-            print(f'Keyword,File type,GitHub URL,File match')
+            print(f'Keyword,File type,ENGitHub URL,File match')
 
+            for entry in searchresults:
+                try:
+                    time.sleep(2)
+                    # each entry is a ContentFile https://pygithub.readthedocs.io/en/latest/github_objects/ContentFile.html#github.ContentFile.ContentFile
+                    path = entry.path
+                    try:
+                        actualfiletype = path.rsplit(sep='.')[1]
+                    except Exception as e:
+                        # Sometimes the string split would find another character, causing an error
+                        actualfiletype = path.rsplit(sep='/')[1] 
+                    print(f'{keyword},{actualfiletype},{entry.download_url},{entry.path}')
+                except Exception as e:
+                    print("Error: ", e)
+            else:
+                for file in searchresults:
+                    path = file.path
+                    try:
+                        actualfiletype = path.rsplit(sep='.')[1]
+                    except Exception as e:
+                        actualfiletype = path.rsplit(sep='/')[1]
+                    print(f'{keyword},{actualfiletype},{file.download_url},{file.path}')
+
+        else:
+            # For queries with an exact file type
+            print("Query with filetype: ", filetype)
+            query = f'"{keyword}" org:{config.egh_orgname} in:file extension:{filetype}'
+            searchresults = eg.search_code(query, order='desc')
+            
+            logger.info(f'Found {searchresults.totalCount} entries(s) with {keyword}')
+            print("Found {} entries(s) with {}", searchresults.totalCount, keyword)
+            print(f'Keyword,File type,ENGitHub URL,File match')
+             
             for entry in searchresults:
                 try:
                     time.sleep(2)
@@ -120,34 +153,20 @@ def search_enterprise_github(keyword, filetype):
                         actualfiletype = path.rsplit(sep='/')[1]
                     print(f'{keyword},{actualfiletype},{file.download_url},{file.path}')
 
-        else:
-            # For queries with an exact file type
-            query = f'"{keyword}" org:{config.gh_orgname} in:file extension:{filetype}'
-            searchresults = g.search_code(query, order='desc')
-            
-            logger.info(f'Found {searchresults.totalCount} entries(s) with {keyword}')
-            print(f'Keyword,File type,GitHub URL,File match')
-             
-            for entry in searchresults:
-                try:
-                    # Try to avoid rate limits
-                    time.sleep(2)
-                    print(f'{keyword},{filetype},{searchresults.html_url},{searchresults.path}')
-                except Exception as e:
-                    print("Error: ", e)
-
 if __name__ == '__main__':
-    which_github = input('Enter Enterprise for Enterprise GitHub, otherwise by default search GitHub: ')
-    keyword = input('Enter biased keyword such as \"master\", \"slave\", \"blacklist\", \"whitelist\": ')
-    filetype = input('Enter extension for files to search within such as \"py\" for Python, \"md\" for Markdown, and enter \"any\" for all file types: ')
-    #keyword = 'whitelist'
+    #which_github = input('Enter Enterprise for Enterprise GitHub, otherwise by default search GitHub: ')
+    #keyword = input('Enter biased keyword such as \"master\", \"slave\", \"blacklist\", \"whitelist\": ')
+    #filetype = input('Enter extension for files to search within such as \"py\" for Python, \"md\" for Markdown, and enter \"any\" for all file types: ')
+    keyword = 'whitelist'
     #keyword = 'slave'
     #filetype = 'any'
+    filetype = 'md'
     #filetype = 'py'
-    if which_github == "Enterprise":
-        search_enterprise_github(keyword, filetype)
-    else:
-        search_github(keyword, filetype)
+    search_enterprise_github(keyword, filetype)
+    # if which_github == "Enterprise":
+    #     search_enterprise_github(keyword, filetype)
+    # else:
+    #     search_github(keyword, filetype)
     
     logging.basicConfig(filename='allsearchresultsinfo.log',
                         encoding='utf-8', 
